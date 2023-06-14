85597996adf6441b9ed2234a9a41c744563c2944
kutmanvizdaimant <kutmanvizdaimant@gmail.com>
2023-06-14 20:12:13 +0600

    users кощтук

--- users/views.py
+++ users/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def say_hello(request):
+    b = 23 + 27
+    return HttpResponse(f'Hello my name is Kutman  {b}')
