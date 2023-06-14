872ad0df40cbe9287f98e14575cf42c3e59f31c7
kutmanvizdaimant <kutmanvizdaimant@gmail.com>
2023-06-14 20:13:11 +0600

    secand lessons

--- users/models.py
+++ users/models.py
@@ -1,4 +1,12 @@
 from django.db import models
 
 # Create your models here.
-# class Usepy
+class Users(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    age = models.IntegerField()
+    phone = models.CharField(max_length=150)
+    email = models.CharField(max_length=150)
+
+    def __str__(self):
+        return f'{self.first_name}  {self.last_name}'
